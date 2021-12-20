0b8cdf41712d679a01508b68a22a5ea79c30a489
Elena <silena003@gmail.com>
2021-12-20 10:18:28 +0300

    Initial commit

---
+++ src/main/java/ru/netology/developers/Donut.java
@@ -0,0 +1,40 @@
+package ru.netology.developers;
+
+public class Donut {
+    private boolean isDonut;
+    private int paidDuration;
+    private Placeholder placeholder;
+    private boolean canPublishFreeCopy;
+
+    public boolean isDonut() {
+        return isDonut;
+    }
+
+    public void setDonut(boolean donut) {
+        isDonut = donut;
+    }
+
+    public int getPaidDuration() {
+        return paidDuration;
+    }
+
+    public void setPaidDuration(int paidDuration) {
+        this.paidDuration = paidDuration;
+    }
+
+    public Placeholder getPlaceholder() {
+        return placeholder;
+    }
+
+    public void setPlaceholder(Placeholder placeholder) {
+        this.placeholder = placeholder;
+    }
+
+    public boolean isCanPublishFreeCopy() {
+        return canPublishFreeCopy;
+    }
+
+    public void setCanPublishFreeCopy(boolean canPublishFreeCopy) {
+        this.canPublishFreeCopy = canPublishFreeCopy;
+    }
+}
---
+++ src/main/java/ru/netology/developers/Placeholder.java
@@ -0,0 +1,4 @@
+package ru.netology.developers;
+
+public class Placeholder {
+}
